44edbf5ea93b83c422846125131c1662da432d62
Sabarish2001 <sabarishb2001@gmail.com>
2024-05-18 08:18:48 +0530

    added three lines into simple_msg_forgit.sh file

--- simple_msg_forgit.sh
+++ simple_msg_forgit.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Learning git is going so far"
+
+echo "Adding a line to check git is recording the differences made compared to last commit"
+echo "And learning git is good as it help me to track the versions of the files"
+echo "So far so good"
