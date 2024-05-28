e41d4e5efa2fe58350e986af8e00233f116ab0db
Sabarish2001 <sabarishb2001@gmail.com>
2024-05-28 18:28:22 +0530

    just removed few lines from passing_args_to_functions.sh

--- passing_args_to_functions.sh
+++ passing_args_to_functions.sh
@@ -10,10 +10,8 @@ function odd_or_even
 		if (( $start %2 == 0 ))
 		then
 			echo "Number is $start and it is even"
-			((even++))
 		else
 			echo "Number is $start and it is odd"
-			((odd++))
 		fi
 		((start++))
 	done
